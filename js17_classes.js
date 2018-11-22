7e96899b14607fa0cc12645a719629fec3fb64fa
lawrence <lawrence.t.yen@gmail.com>
2018-11-21 17:41:36 -0800

    Completed assignment17

---
+++ js17_classes.js
@@ -0,0 +1,13 @@
+var Clothes = class {
+    constructor(name, size, price){
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+}
+// add code below
+class Shirt extends Clothes {
+  constructor() {
+    super(shirtName, shirtSize, shirtPrice);
+  };
+}
